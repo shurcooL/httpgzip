3df7b3563cfe5626e5b109992a6c0b07edadfaa4
Quinn Slack <qslack@qslack.com>
2017-08-08 00:42:51 -0700

    fix failing test and add new test for DirListing

--- fs_test.go
+++ fs_test.go
@@ -51,6 +51,30 @@ func TestServeContent_detectContentType(t *testing.T) {
 	}
 }
 
+// Test that no dir listing is shown if the DirListing option is false.
+func TestFileServer_noDirListing(t *testing.T) {
+	fs := httpfs.New(mapfs.New(map[string]string{
+		"foo.txt": "Hello world",
+	}))
+	ts := httptest.NewServer(http.StripPrefix("/bar/", httpgzip.FileServer(fs, httpgzip.FileServerOptions{})))
+	defer ts.Close()
+	res, err := http.Get(ts.URL + "/bar/")
+	if err != nil {
+		t.Fatalf("Get: %v", err)
+	}
+	defer res.Body.Close()
+	if want := http.StatusForbidden; res.StatusCode != want {
+		t.Fatalf("got status %d, want %d", res.StatusCode, want)
+	}
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("ReadAll: %v", err)
+	}
+	if want := "403 Forbidden\n"; string(b) != want {
+		t.Fatalf("got body %q, want %q", b, want)
+	}
+}
+
 // Test that there are no infinite redirects at root path even if the entire
 // req.URL.Path is stripped, e.g., via an overly aggressive http.StripPrefix.
 // See https://github.com/shurcooL/httpgzip/pull/3
@@ -59,7 +83,7 @@ func TestFileServer_implicitLeadingSlash(t *testing.T) {
 	fs := httpfs.New(mapfs.New(map[string]string{
 		"foo.txt": "Hello world",
 	}))
-	ts := httptest.NewServer(http.StripPrefix("/bar/", httpgzip.FileServer(fs, httpgzip.FileServerOptions{})))
+	ts := httptest.NewServer(http.StripPrefix("/bar/", httpgzip.FileServer(fs, httpgzip.FileServerOptions{DirListing: true})))
 	defer ts.Close()
 	get := func(suffix string) string {
 		res, err := http.Get(ts.URL + suffix)
